f46cd54947ab471c9db8fb4fff05374066ea4eef
Vagrant Default User <vagrant@stretch.localdomain>
2017-09-29 10:46:46 +0000

    fin de matinée j4

---
+++ src/OverwatchHeroBundle/Command/HeroFixturesCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace OverwatchHeroBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use OverwatchHeroBundle\Entity\Hero;
+
+
+class HeroFixturesCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('Hero:fixtures')
+            ->setDescription('Chargement du jeud\'essai')
+           
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
+
+        $hero=new Hero();
+        $hero->setName("Hanzo");
+        $em->persist($hero);
+
+        $hero=new Hero();
+        $hero->setName("D.Va");
+        $em->persist($hero);
+        
+        $hero=new Hero();
+        $hero->setName("Genji");
+        $em->persist($hero);
+        
+        $hero=new Hero();
+        $hero->setName("Domm Fist");
+        $em->persist($hero);
+        
+
+        $em->flush();
+
+        $output->writeln('<info>OK</info>');
+    }
+
+}
--- src/OverwatchHeroBundle/Controller/DetailController.php
+++ src/OverwatchHeroBundle/Controller/DetailController.php
@@ -5,6 +5,7 @@ namespace OverwatchHeroBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use OverwatchHeroBundle\Repository\OverwatchHeroRepository;
+use OverwatchHeroBundle\Entity\Category;
 
 
 class DetailController extends Controller
@@ -14,8 +15,9 @@ class DetailController extends Controller
      */
     public function detailAction($overwatchHeroId)
     {
-        $overwatchHeroRepository = new OverwatchHeroRepository();
-        $hero = $overwatchHeroRepository->findById($overwatchHeroId);
+        //$overwatchHeroRepository = new OverwatchHeroRepository();
+        $overwatchHeroRepository = $this->getDoctrine()->getRepository(Category::class);
+        $hero = $overwatchHeroRepository->findAll();
         
         return $this->render('OverwatchHeroBundle:Hero:detail.html.twig', [
             'hero' => $hero,
