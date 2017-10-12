93abe02853cc0f3d450880ce39415faac7ea49f3
Vagrant Default User <vagrant@stretch.localdomain>
2017-10-12 15:56:18 +0000

    edit user role fixé

--- src/UserBundle/Form/UserType.php
+++ src/UserBundle/Form/UserType.php
@@ -5,6 +5,8 @@ namespace UserBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\CallbackTransformer;
 
 class UserType extends AbstractType
 {
@@ -13,7 +15,21 @@ class UserType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('email')->add('password')->add('roles');
+        $builder->add('email')->add('password');
+        $builder->add('roles', TextType::class);
+
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesAsArray) {
+                    // transform the array to a string
+                    return implode(', ', $rolesAsArray);
+                },
+                function ($rolesAsString) {
+                    // transform the string back to an array
+                    return explode(', ', $rolesAsString);
+                }
+            ))
+        ;
     }
     
     /**
